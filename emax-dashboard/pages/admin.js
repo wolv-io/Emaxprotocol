63528f59ac825fc4a58331fe73a50b15eb87da68
wolf-invest <johnjamie.hotmail@gmail.com>
2025-06-01 02:13:09 +0100

    Add files via upload

---
+++ emax-dashboard/pages/admin.js
@@ -0,0 +1,4 @@
+
+export default function AdminDashboard() {
+  return <h1>Admin Dashboard - Only visible to admin@example.com</h1>;
+}
---
+++ emax-dashboard/pages/login.js
@@ -0,0 +1,4 @@
+
+export default function LoginPage() {
+  return <h1>Login Page (Firebase Auth goes here)</h1>;
+}
